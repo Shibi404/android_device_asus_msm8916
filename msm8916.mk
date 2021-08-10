5beafb54f24cb984afff9c8560fe5eee11899348
Shibi404 <shibisbs123@gmail.com>
2021-08-10 17:23:46 +0530

    msm8916-common: Remove support for OpenGapps and PixelGapps
    Signed-off-by: Shibi404 <shibisbs123@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -14,19 +14,6 @@
 # limitations under the License.
 #
 
-# OpenGapps
-ifeq ($(shell test -e vendor/opengapps/build/opengapps-packages.mk && echo -n yes), yes)
-    $(call inherit-product, device/asus/msm8916-common/opengapps.mk)
-endif
-
-# PixelGapps
-ifeq ($(shell test -e vendor/gapps/config.mk && echo -n yes), yes)
-    TARGET_GAPPS_ARCH=arm64
-    TARGET_DENSITY=xhdpi
-    IS_PHONE=true
-    $(call inherit-product, vendor/gapps/config.mk)
-endif
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
