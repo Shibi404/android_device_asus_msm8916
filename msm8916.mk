0e3559fdb8f174f7a79e948c664c57e0d601ef3a
Shibi404 <shibisbs123@gmail.com>
2021-09-14 19:38:53 +0530

    msm8916-common: Nuke Light hal * ALready defined in Z010D Tree
    Signed-off-by: Shibi404 <shibisbs123@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -224,10 +224,6 @@ PRODUCT_PACKAGES += \
     keystore.msm8916
 endif
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.asus_8953
-
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
