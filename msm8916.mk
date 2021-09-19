432b1d4b565643818c1d8df038f537adb4506fbf
Shibi404 <shibisbs123@gmail.com>
2021-09-19 19:25:48 +0530

    msm8916-common: Uprev GNSS Hal to 2.0 Signed-off-by: Shibi404 <shibisbs123@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -166,8 +166,8 @@ PRODUCT_PACKAGES += \
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+    android.hardware.gnss@2.0-impl-qti \
+    android.hardware.gnss@2.0-service-qti
 
 # GPS
 PRODUCT_PACKAGES += \
